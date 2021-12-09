b78909e4d3049534f3d5698df313a815c4e291ed
Renchen Sun <rsun@wish.com>
2021-12-09 22:35:30 +0000

    [i18n] fixed

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+PATH=/home/rsun/ContextLogic/clroot/ci/bin:/home/rsun/.toolbox/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/home/rsun/.pyenv/shims:/home/rsun/.pyenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/rsun/.nvm/versions/node/v15.3.0/bin:/Users/renchen/go/bin/:/home/rsun/.pyenv/bin:/home/rsun/.npm-global/lib/node_modules/yarn/bin
 set -e
 cd ${REPO_DIR}
 docker build . -t strings_sync 
